86bf5b8aaf865b16cc807cf6c65ee48949464a3d
Bruno Iaguy <93688250+iaguy@users.noreply.github.com>
2022-04-23 22:33:21 -0300

    update project

---
+++ script/index01.js
@@ -0,0 +1,16 @@
+function carregar(){
+   var msg = window.document.getElementById('msg')
+   var img = window.document.getElementById('imagem')
+   var data = new Date()
+   var hora = data.getHours()
+   msg.innerHTML = `Agora são ${hora} horas. `
+   if(hora >= 0 && hora <12){
+       img.src = 'style/manha.png'
+   }else if(hora >= 12 && hora <18){
+       img.src = 'style/tarde.png'
+       document.body.style.background = '#DFA703'
+   }else{
+        img.src = 'style/noite.png'
+        document.body.style.background = '#0C112B'
+   }
+}
