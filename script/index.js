0f5bae04c83a20d1a8baadf07bf21b4ff13cc5f1
Bruno Iaguy <93688250+iaguy@users.noreply.github.com>
2022-04-24 22:00:29 -0300

    atualização de pages e estilos

---
+++ script/index.js
@@ -0,0 +1,42 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('nasc')
+    var res = window.document.querySelector('div#res')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if(fano.value.length == 0 || Number(fano.value) > ano){
+    window.alert('[ERRO] Verifique os dados e tente novamente!')  
+    }else{
+      var fsex = window.document.getElementsByName('radsex')
+      var idade = ano - Number(fano.value)
+      var gen = ""
+      if(fsex[0].checked){
+          gen = 'Homem'
+          if(idade > 0 && idade <15){
+            img.setAttribute('src', 'style/crianca-m.png')
+          }else if (idade > 15 && idade < 21){
+            img.setAttribute('src', 'style/jovem-m.png')
+          }else if(idade > 21 && idade < 50){
+            img.setAttribute('src', 'style/adulto-m.png')
+          }else{
+            img.setAttribute('src', 'style/idoso-m.png')
+          }
+      }else if (fsex[1].checked){
+          gen = 'Mulher'
+          if(idade > 0 && idade <15){
+            img.setAttribute('src', 'style/crianca-f.png')
+          }else if (idade > 15 && idade < 21){
+            img.setAttribute('src', 'style/jovem-f.png')
+          }else if(idade > 21 && idade < 50){
+            img.setAttribute('src', 'style/adulto-f.png')
+          }else{
+            img.setAttribute('src', 'style/idoso-f.png')
+        }
+      }
+      res.style.textAlign = 'center'
+      res.innerHTML = `Detectei <strong>${gen}</strong> com <strong>${idade}</strong> anos.`
+      res.appendChild(img)
+      
+    }
+   }
--- script/index01.js
+++ script/index01.js
@@ -3,14 +3,21 @@ function carregar(){
    var img = window.document.getElementById('imagem')
    var data = new Date()
    var hora = data.getHours()
-   msg.innerHTML = `Agora são ${hora} horas. `
+   var min = data.getMinutes()
+   var seg = data.getSeconds()
+   var tempo = hora+":"+min+":"+seg
+   document.getElementById('msg').innerHTML = `Olá são: ${tempo}`
+   setInterval(carregar, 1000)
    if(hora >= 0 && hora <12){
        img.src = 'style/manha.png'
+       document.getElementById("saudacao").innerHTML = 'Bom dia!'
    }else if(hora >= 12 && hora <18){
        img.src = 'style/tarde.png'
+       document.getElementById("saudacao").innerHTML = 'Boa tarde!'
        document.body.style.background = '#DFA703'
    }else{
         img.src = 'style/noite.png'
         document.body.style.background = '#0C112B'
+        document.getElementById("saudacao").innerHTML = 'Boa Noite!'
    }
 }
